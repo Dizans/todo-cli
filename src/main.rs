e0e7f71d22dfe7c94c57127df767f1d9832343a8
Dizans <dizansyu@gmail.com>
2020-03-09 15:35:18 +0800

    implement todo-cli

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-// TODO:
-// 1. cli: 接收命令
-// 2. 定义结构
-// 3. 存成本地的 json 数据
-use std::fs;
-use std::io::{self, prelude::*, BufReader};
+use std::fs::{File, OpenOptions};
+use std::io::{prelude::*, BufReader};
 use std::path::Path;
+use std::fmt;
 
 use clap::{Arg, App, SubCommand};
-use chrono;
-use chrono::prelude::*;
+use chrono::{prelude::*, Local, Duration};
+use anyhow::Result;
+use anyhow::anyhow;
+use ansi_term::Color::Red;
 
 use serde::{self, Deserialize, Serialize};
-use serde_json;
+use dirs::home_dir;
+
 
 type LocalDT = chrono::DateTime<chrono::Local>; 
 
@@ -20,6 +20,12 @@ struct TODOItem{
     create_time: LocalDT,
     check_time: Option<LocalDT>,
     content: String,
+}
+
+impl fmt::Display for TODOItem{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ", self.content)
+    }
 }
 
 #[allow(dead_code)]
@@ -36,7 +42,6 @@ impl TODOItem{
         let now = Local::now();
         self.check_time = Some(now);
     }
-
 }
 
 
@@ -55,22 +60,77 @@ impl<T: AsRef<Path>> DB<T> {
 
     pub fn load(&mut self){
         if !self.path.as_ref().exists(){
-            fs::File::create(&self.path).expect("create DB file failed");
+            File::create(&self.path).expect("create DB file failed");
         } 
-        let db_file = fs::File::open(&self.path).expect("read DB file faild");
+        let db_file = File::open(&self.path).expect("read DB file faild");
         let buf = BufReader::new(db_file);
         for line in buf.lines(){
             let item: TODOItem = serde_json::from_str(&line.unwrap())
                 .expect("invalid string in DB file");
             self.items.push(item);
         }
-
     }
 
     pub fn save(&self){
+        let mut file = OpenOptions::new()
+                    .write(true)
+                    .open(&self.path)
+                    .expect("failed to open DB file");
+        self.items.iter()
+                  .for_each(|x| {
+                      serde_json::to_writer(&file, x).unwrap();
+                      file.write("\n".as_bytes()).unwrap();
+                   });
+    }
+
+    pub fn insert(&mut self, item: TODOItem){
+        self.items.push(item);
+    }
+
+    pub fn show_todo_today(&mut self){
+        self.get_todays_todo()
+                   .iter()
+                   .enumerate()
+                   .for_each(|(i, x)| {
+                       let status = if x.check_time.is_some() { 
+                                        Red.paint("★").to_string() 
+                                    } else {
+                                        "☆".to_string()
+                                    };
+                       println!("{0:>5}. {2}  {1}", i+1,x, status);
+                    });
+    }
     
+    pub fn check_todo(&mut self, index: usize) -> Result<()>{
+        let mut todos = self.get_todays_todo();
+        let item = todos.iter_mut().nth(index);
+        match item{
+            Some(v) => v.check(),
+            None =>  return Err(anyhow!("invalid index")),
+        }
+        Ok(())
+    }
+
+    fn get_todays_todo(&mut self) -> Vec<&mut TODOItem>{
+        self.get_todos_in_last_n_days(1)    
+    }
+
+    fn get_todos_in_last_n_days(&mut self, n: i64) -> Vec<&mut TODOItem>{
+        assert!(n >= 1);
+        let today = Local::today()
+                    .and_time(NaiveTime::from_hms(0,0,0))
+                    .unwrap();
+        let n = n-1;
+        let duration = Duration::days(-n);
+        let start_day = today - duration;
+
+        self.items
+            .iter_mut()
+            .filter(|x| x.create_time > start_day)
+            .collect()
     }
 }
+
 
 fn main() {
     let matchs = App::new("todo-cli")
@@ -79,13 +139,32 @@ fn main() {
             SubCommand::with_name("add")
             .arg(Arg::with_name("item").required(true))
         )
+        .subcommand(
+            SubCommand::with_name("check")
+            .arg(Arg::with_name("index").required(true))
+        )
         .get_matches();
     
+    let mut home_dir = home_dir().expect("can't get home dir");
+    home_dir.push("db_file");
+
+    let mut db = DB::new(home_dir);
+    db.load();
+
     if let Some(ref matchs) = matchs.subcommand_matches("add"){
         let content = matchs.value_of("item").unwrap();
         let item = TODOItem::new(content.to_owned());
-        println!("add item: {}", content);
-        println!("{:?}", item);
+        db.insert(item);
     }
 
+    if let Some(ref matchs) = matchs.subcommand_matches("check"){
+        let index = matchs.value_of("index")
+                          .unwrap()
+                          .parse::<usize>()
+                          .unwrap_or_else(|_| panic!("invalid index"));
+        let index = index - 1;
+        db.check_todo(index).unwrap();
+    }
+    db.show_todo_today();
+    db.save();
 }
